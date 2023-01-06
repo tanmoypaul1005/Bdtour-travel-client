6f703e0aa0f1a8630c638f9fd26b41fe08f249dc
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-06 15:07:15 +0600

    aaa

--- src/Page/BookingCard/BookingCard.jsx
+++ src/Page/BookingCard/BookingCard.jsx
@@ -3,25 +3,28 @@ import React from 'react';
 const BookingCard = () => {
     return (
         <div>
-            <div className="max-w-sm w-full lg:max-w-full lg:flex">
-                <div className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" style="background-image: url('/img/card-left.jpg')" title="Woman holding a mug">
-                </div>
-                <div className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                    <div className="mb-8">
-                        <p className="text-sm text-gray-600 flex items-center">
-                            <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
-                            </svg>
-                            Members only
-                        </p>
-                        <div className="text-gray-900 font-bold text-xl mb-2">Can coffee make you a better developer?</div>
-                        <p className="text-gray-700 text-base">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.</p>
-                    </div>
-                    <div className="flex items-center">
-                        <img className="w-10 h-10 rounded-full mr-4" src="/img/jonathan.jpg" alt="Avatar of Jonathan Reinink" />
-                        <div className="text-sm">
-                            <p className="text-gray-900 leading-none">Jonathan Reinink</p>
-                            <p className="text-gray-600">Aug 18</p>
+            <div class="pt-6 pb-12 bg-gray-300">
+                <div id="card" class="">
+                    <h2 class="text-center font-serif  uppercase text-4xl xl:text-5xl">Recent Articles</h2>
+
+                    <div class="container w-100 lg:w-4/5 mx-auto flex flex-col">
+
+                        <div v-for="card in cards" class="flex flex-col md:flex-row overflow-hidden
+                                        bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2">
+
+                            <div class="h-64 w-auto md:w-1/2">
+                                <img class="inset-0 h-full w-full object-cover object-center"  />
+                            </div>
+
+                            <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
+                                <h3 class="font-semibold text-lg leading-tight truncate">sss</h3>
+                                <p class="mt-2">
+                                  aaaaaaaaaaaaaaaaa
+                                </p>
+                                <p class="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
+                                  aaa
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
