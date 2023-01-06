6d6306a127c370e4287247acc990296d85be0159
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-06 17:21:55 +0600

    j

--- src/Page/NavBar/NavBar.jsx
+++ src/Page/NavBar/NavBar.jsx
@@ -56,10 +56,10 @@ const NavBar = () => {
                     </a>
 
                     <a
-                      href="/"
+                      href="/booking-cart"
                       className="text-gray-300 hover:bg-[#2257AA] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                     >
-                      Reports
+                     Booking-cart
                     </a>
                   </div>
                 </div>
