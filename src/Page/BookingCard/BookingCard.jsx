b46cf549f5a57671c599d96a9ec717b89265e305
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-06 17:28:18 +0600

    j

--- src/Page/BookingCard/BookingCard.jsx
+++ src/Page/BookingCard/BookingCard.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
+import { useGetBookingQuery } from '../../App/Features/Booking/BokingSlice';
 
 const BookingCard = () => {
+
+    const {}=useGetBookingQuery();
+
     return (
         <div>
-            <div class="pt-6 pb-12 bg-gray-300">
-                <div id="card" class="">
+            <div className="pt-6 pb-12 bg-gray-300">
+                <div id="card" className="">
                     <h2 class="text-center font-serif  uppercase text-4xl xl:text-5xl">Recent Articles</h2>
 
-                    <div class="container w-100 lg:w-4/5 mx-auto flex flex-col">
+                    <div className="container w-100 lg:w-4/5 mx-auto flex flex-col">
 
-                        <div v-for="card in cards" class="flex flex-col md:flex-row overflow-hidden
+                        <div v-for="card in cards" className="flex flex-col md:flex-row overflow-hidden
                                         bg-white rounded-lg shadow-xl  mt-4 w-100 mx-2">
 
-                            <div class="h-64 w-auto md:w-1/2">
-                                <img class="inset-0 h-full w-full object-cover object-center"  />
+                            <div className="h-64 w-auto md:w-1/2">
+                                <img className="inset-0 h-full w-full object-cover object-center"  />
                             </div>
 
-                            <div class="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
-                                <h3 class="font-semibold text-lg leading-tight truncate">sss</h3>
-                                <p class="mt-2">
+                            <div className="w-full py-4 px-6 text-gray-800 flex flex-col justify-between">
+                                <h3 className="font-semibold text-lg leading-tight truncate">sss</h3>
+                                <p className="mt-2">
                                   aaaaaaaaaaaaaaaaa
                                 </p>
-                                <p class="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
+                                <p className="text-sm text-gray-700 uppercase tracking-wide font-semibold mt-2">
                                   aaa
                                 </p>
                             </div>
