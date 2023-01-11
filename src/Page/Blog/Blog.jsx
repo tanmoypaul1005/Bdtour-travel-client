3089b7b2f08edfba5392a019c40e94165b5f2f92
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-01-11 23:04:04 +0600

    add blog

--- src/Page/Blog/Blog.jsx
+++ src/Page/Blog/Blog.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { useGetBlogQuery } from '../../App/Features/Blog/BlogSlice';
 import TourPackageCardShimmer from '../../Components/Card/TourPackageCardShimmer';
+import Error from '../../Components/Error';
 import BlogCard from './BlogCard';
 
 const Blog = () => {
 
-    const {data, isLoading, isError}=useGetBlogQuery();
+    const { data, isLoading, isError } = useGetBlogQuery();
 
     let content = null;
 
     if (isLoading) {
         content = <TourPackageCardShimmer />
-      }
+    }
+
+    if(!isLoading && isError){
+        content = <Error message="There was an error" />;
+    }
+
 
     return (
         <div>
-            <BlogCard/>
+            <BlogCard />
         </div>
     );
 };
